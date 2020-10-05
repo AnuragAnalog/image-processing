055b635ea191bc0e86d90530a5f4eb5ef8e5cfa6
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-10-05 14:28:55 +0530

    added linear transform function

--- transforms.py
+++ transforms.py
@@ -2,6 +2,15 @@
 
 import numpy as np
 
+def linear_transform(image, L=256):
+    min_r = np.min(image)
+    max_r = np.max(image)
+
+    transformed_image = ((image - min_r) / (max_r - min_r)) * (L - 1)
+    transformed_image = np.round(transformed_image)
+
+    return transformed_image.astype('int32')
+
 def preprocess(image):
     if not isinstance(image, np.ndarray):
         raise TypeError("only numpy.ndarray dtypes are expected")
@@ -18,7 +27,10 @@ def negative(image, L=256):
 def log_transform(image, c=1):
     image = preprocess(image)
 
-    return c * np.log(1 + image)
+    new_image = c * np.log(1 + image)
+    new_image = linear_transform(new_image)
+
+    return new_image
 
 def identity(image):
     image = preprocess(image)
@@ -28,7 +40,10 @@ def identity(image):
 def power_transform(image, gamma=1, c=1):
     image = preprocess(image)
 
-    return c * np.power(image, gamma)
+    new_image = c * np.power(image, gamma)
+    new_image = linear_transform(new_image)
+
+    return new_image
 
 def piecewise_transform(image, r1=64, r2=196, s1=64, s2=196, L=256):
     image = preprocess(image)
@@ -39,6 +54,6 @@ def piecewise_transform(image, r1=64, r2=196, s1=64, s2=196, L=256):
         slope, intercept = np.polyfit(x=point[0], y=point[1], deg=1)
 
         mask = np.logical_and((point[0][0] <= image), (image <= point[0][0]))
-        image[mask] = np.floor(slope * image[mask] + intercept)
+        image[mask] = np.round(slope * image[mask] + intercept)
 
     return image
